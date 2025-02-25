f5d2a667ab8c5a65a19b2de96200ae94e61878da
Pine Netcharussaeng <pinetcht@gmail.com>
2025-02-24 16:29:25 -0800

    final commit fr

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,11 +8,11 @@ export const Experience = ({company, date, role, location}) => {
 
         <div className="exp">
             <div className="expHeader">
-            <h2 className="expHeader">{company}</h2>
+            <h2 className="expHeader">{role}</h2>
             <h3 className="expHeader"> {date} </h3>
             </div>
             <div className="expHeader">
-            <h3>{role}</h3>
+            <h3>{company}</h3>
             <h3>{location}</h3>
             </div>
             
